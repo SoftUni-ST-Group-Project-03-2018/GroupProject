1605be45119a85a8c61144bab4fb1e7771561189
Kavier <kavierbg@gmail.com>
2018-03-10 17:26:27 +0200

    Editing

--- intro-and-basic-syntax/Problem 3. MilesToKilometres/Program.cs
+++ intro-and-basic-syntax/Problem 3. MilesToKilometres/Program.cs
@@ -7,13 +7,13 @@ namespace MilesToKilometres
         static void Main(string[] args)
         {
             //We recieve from the Console the distance in miles -> an integer; 
-            double a = double.Parse(Console.ReadLine());
+            double inputInMiles = double.Parse(Console.ReadLine());
 
-            //We calculate the distance from miles to kilometers using the formula 1 mile = 1.60934 km
-            double km = a * 1.60934;
+            //We calculate the distance from miles to kilometers using the formula: 1 mile = 1.60934 km
+            double distanceInKilometers = inputInMiles * 1.60934;
 
             //The Console is printing our result formatted to the 2nd character after the decimal point
-            Console.WriteLine($"{km:f2}");
+            Console.WriteLine($"{distanceInKilometers:f2}");
         }
     }
 }
