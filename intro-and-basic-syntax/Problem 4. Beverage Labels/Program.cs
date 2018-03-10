ab12d5253e5ec87b64f5b23be3ffbcbe152c2f1d
TeodorKunchev <toto___2001@abv.bg>
2018-03-10 12:43:17 +0200

    Teodor make comments

--- intro-and-basic-syntax/Problem 4. Beverage Labels/Program.cs
+++ intro-and-basic-syntax/Problem 4. Beverage Labels/Program.cs
@@ -6,12 +6,17 @@ namespace Problem_4._Beverage_Labels
     {
         static void Main(string[] args)
         {
+			//Getting params for fizzy drink
             string beverage = Console.ReadLine();
             int volume = int.Parse(Console.ReadLine());
             int energy = int.Parse(Console.ReadLine());
             int sugar = int.Parse(Console.ReadLine());
+			
+			//Calculating energy and sugar
             double allEnergy = (double)volume * energy / 100;
             double allSugar = (double)volume * sugar / 100;
+			
+			//Printing each on new line
             Console.WriteLine($"{volume}ml {beverage}:\r\n{allEnergy}kcal, {allSugar}g sugars");
         }
     }
