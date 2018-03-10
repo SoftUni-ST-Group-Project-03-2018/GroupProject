94c570a7f50ceba3f64a55b8915fb670faf8ee28
Kavier <kavierbg@gmail.com>
2018-03-10 13:31:43 +0200

    Changing variables

--- intro-and-basic-syntax/Problem 5. Character Stats/Program.cs
+++ intro-and-basic-syntax/Problem 5. Character Stats/Program.cs
@@ -6,16 +6,16 @@ namespace Problem_5._Character_Stats
     {
         static void Main(string[] args)
         {
-			//  reading the input data from the console
+            //  reading the input data from the console
             var name = Console.ReadLine();
-            var healthFull = int.Parse(Console.ReadLine());
-            var healthEmpty = int.Parse(Console.ReadLine());
-            var energyFull = int.Parse(Console.ReadLine());
-            var energyEmpty = int.Parse(Console.ReadLine());
-			// write the output with place holders and new string
+            var availableHealth = int.Parse(Console.ReadLine());
+            var healthRange = int.Parse(Console.ReadLine());
+            var availableEnergy = int.Parse(Console.ReadLine());
+            var energyRange = int.Parse(Console.ReadLine());
+            // write the output with place holders and new string
             Console.WriteLine($"Name: {name}");
-            Console.WriteLine("Health: |{0}{1}|", new string('|', healthFull), new string('.', healthEmpty - healthFull));
-            Console.WriteLine("Energy: |{0}{1}|", new string('|', energyFull), new string('.', energyEmpty - energyFull));
+            Console.WriteLine("Health: |{0}{1}|", new string('|', availableHealth), new string('.', healthRange - availableHealth));
+            Console.WriteLine("Energy: |{0}{1}|", new string('|', availableEnergy), new string('.', energyRange - availableEnergy));
 
         }
     }
