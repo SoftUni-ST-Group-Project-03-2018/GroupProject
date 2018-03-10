c75648fa255915a5fba9a0d21c7b411aa1afb8ff
SimeonVSimeonov <simeonsimeonoff@icloud.com>
2018-03-10 11:02:09 +0200

    add a solution 4

---
+++ intro-and-basic-syntax/Problem 4. Beverage Labels/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Problem_4._Beverage_Labels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string beverage = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            int energy = int.Parse(Console.ReadLine());
+            int sugar = int.Parse(Console.ReadLine());
+            double allEnergy = (double)volume * energy / 100;
+            double allSugar = (double)volume * sugar / 100;
+            Console.WriteLine($"{volume}ml {beverage}:\r\n{allEnergy}kcal, {allSugar}g sugars");
+        }
+    }
+}
